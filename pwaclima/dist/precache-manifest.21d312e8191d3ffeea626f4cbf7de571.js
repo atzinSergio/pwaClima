125a26d1878468d7ad0d0d51ebb4dd30c2a15781
HugoHR0 <hugo09@micorreo.upp.edu.mx>
2021-10-29 09:37:27 -0500

    version 1.0.0

---
+++ pwaclima/dist/precache-manifest.21d312e8191d3ffeea626f4cbf7de571.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eaefbb0e31318e9f3c",
+    "url": "/css/app.d1926b2c.css"
+  },
+  {
+    "revision": "c7838a223d0a1ac2d570",
+    "url": "/css/chunk-vendors.adb59f75.css"
+  },
+  {
+    "revision": "1094ecf233a1da71011951acf270c82f",
+    "url": "/img/electrica_dark.1094ecf2.png"
+  },
+  {
+    "revision": "db0542faafd00e9a9f813fea240741e3",
+    "url": "/img/fondoDia.db0542fa.png"
+  },
+  {
+    "revision": "0dcd515737afd6f4ebd1624d2b71e3be",
+    "url": "/img/fondoNoche.0dcd5157.png"
+  },
+  {
+    "revision": "86c4942078dbaad8dfc35c6a6694c170",
+    "url": "/img/lluvia_dark.86c49420.png"
+  },
+  {
+    "revision": "318737bb6dad9bc2025938c293130321",
+    "url": "/img/nevada_dark.318737bb.png"
+  },
+  {
+    "revision": "d8a928c2abb30c2f3bf9e011a186efad",
+    "url": "/img/parcialnublado_dark.d8a928c2.png"
+  },
+  {
+    "revision": "84db6c1420453bc12540c284b7335d35",
+    "url": "/img/soleado_dark.84db6c14.png"
+  },
+  {
+    "revision": "e298399c91e9ed6eeb31484960306f6c",
+    "url": "/img/tempMax.e298399c.png"
+  },
+  {
+    "revision": "5c3886897b8a34428499fd78acb8c854",
+    "url": "/img/tempMin.5c388689.png"
+  },
+  {
+    "revision": "d8898daf78bc71fdf4c5a0b01200ac92",
+    "url": "/img/viento_dark.d8898daf.png"
+  },
+  {
+    "revision": "9edde7d08fd646823003f97aa11155ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5eaefbb0e31318e9f3c",
+    "url": "/js/app.7acdb4ca.js"
+  },
+  {
+    "revision": "c7838a223d0a1ac2d570",
+    "url": "/js/chunk-vendors.ae4c111c.js"
+  },
+  {
+    "revision": "1132cf5a4c181744c761aa46d99c5e00",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
